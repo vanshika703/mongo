2237065fe2ad79126e482ef3b360bf5f0f4c7023
vanshika703 <vanshikabhojnagarwala@gmail.com>
2020-03-19 23:00:07 +0530

    1st try

---
+++ app.js
@@ -0,0 +1,32 @@
+var mongo = require('mongodb')
+
+var MongoClient = mongo.MongoClient
+var url = "mongodb+srv://vanshika703:vanshika@cluster0-ndrdb.mongodb.net/test?retryWrites=true&w=majority"
+
+MongoClient.connect(url, (err,db)=>{
+    if(err) throw(err)
+    console.log("db connected")
+
+    var dbo = db.db("forms")
+    dbo.createCollection("form1", (err,res)=>{
+        if (err) throw err
+        console.log("Collection created")
+        db.close()
+        })
+
+})
+
+MongoClient.connect(url, (err,db)=>{
+    if(err) throw err
+    var dbo = db.db("forms")
+    var myobj = 
+    {
+        name : "Vanshika",
+        age : 20
+    }
+    dbo.collection("forms").insertOne(myobj, (err,res)=>{
+        if(err) throw err
+        console.log("1 doc inserted")
+        db.close()
+    })
+})
